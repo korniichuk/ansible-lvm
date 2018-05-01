89c6c971f7576f8d1e98451fa500fd71350776c4
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2018-05-01 12:50:14 +0200

    [ansible-lvm][fabric] Improve readability

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,11 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-
-"""ansible-lvm"""
+# Name: ansible-lvm
 
 from fabric.api import local
 
 def git():
-    """Setup Git"""
+    """Configure Git"""
 
     local("git remote rm origin")
     local("git remote add origin https://korniichuk@github.com/korniichuk/ansible-lvm.git")
